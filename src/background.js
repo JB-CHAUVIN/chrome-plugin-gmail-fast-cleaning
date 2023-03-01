9cef17106fef8742c1b4cd6689e372b760c37f3f
jbchauvin <jean-baptiste.chauvin@cegedim-sante.com>
2023-03-01 14:36:36 +0100

    feat : plugin is working

--- src/background.js
+++ src/background.js
@@ -1,11 +1,27 @@
+console.log('Load Gmail extension')
+
 function getSelectionText() {
 	const email = (document.querySelector("span.go").innerText || '').replace('<', '').replace('>', '');
 	const selectedText = window.getSelection().toString();
   return email || selectedText;
 }
 
+function handleFindEmails(variables) {
+	const element = document.querySelector("input.gb_jf");
+	console.log('Trying to find element', element, variables.selectionText)
+	if (element) {
+		element.value = variables.selectionText;
+	}
+
+	document.querySelector('button.gb_sf').click()
+	setTimeout(() => {
+		const clickable = document.querySelector('.T-Jo-auh')
+		clickable.click()
+	}, 2000)
+}
+
 function actionDeleteEmails() {
-	var selectionText = ''
+	let selectionText = ''
 
 	chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
 	  const tabId = tabs[0].id;
@@ -21,19 +37,15 @@ function actionDeleteEmails() {
 
 	    chrome.scripting.executeScript({
 		    target: { tabId: tabId },
-		    function: () => {
-				const element = document.querySelector("#gs_lc50 > input:nth-child(1)");
-				console.log('selectionText')
-				if (element) {
-					element.innerText = 'selectionText';
-				}
-			}
+			args: [{selectionText}],
+		    function: handleFindEmails
 		  });
 	  });
 	});
 }
 
 chrome.commands.onCommand.addListener((command) => {
+	console.log('Command:', command)
   if(command === 'delete-emails') {
   	actionDeleteEmails()
   }
